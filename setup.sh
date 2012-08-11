3e87058577ade0138583c19d5642113def9f4f6a
antonio lorusso <antonio.lorusso@gmail.com>
2012-08-11 02:27:21 +0200

    added bundler and unicorn with openshift hooks for rails

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+OPENSHIFT_ACTION_HOOKS_DIR=$OPENSHIFT_REPO_DIR/.openshift/action_hooks
 cd $OPENSHIFT_TMP_DIR
-
 
 ### yaml
 # get and compile
@@ -35,7 +35,58 @@ rm -rf ruby*
 
 ### export the bin directory
 export PATH=$OPENSHIFT_RUNTIME_DIR/bin:$PATH
-# install bundler
 cd $OPENSHIFT_REPO_DIR
-gem install bundler
 
+### bundler
+gem install bundler
+# setup the pre-build script
+echo > $OPENSHIFT_ACTION_HOOKS_DIR/pre-build
+$OPENSHIFT_ACTION_HOOKS_DIR/pre-build << 'EOF'
+data="${OPENSHIFT_DATA_DIR}/assets"
+assets="${OPENSHIFT_REPO_DIR}/public"
+# Make the data directory if it doesn't exist
+if [ ! -d "$data" ]; then
+  mkdir $data;
+fi
+# Remove the assets directory if it's empty
+find $assets/assets -maxdepth 0 -type d -empty -delete 2> /dev/null
+# Try to make the symlink
+ln -s $data $assets 2> /dev/null
+if [ $? -gt 0 ]; then
+  echo "Unable to create symlink to assets directory, using existing directory in repository"
+fi
+EOF
+# setup the build script
+echo > $OPENSHIFT_ACTION_HOOKS_DIR/build
+$OPENSHIFT_ACTION_HOOKS_DIR/build << 'EOF'
+pushd ${OPENSHIFT_REPO_DIR} > /dev/null
+bundle install --deployment
+popd > /dev/null
+EOF
+# setup the deploy script
+echo > $OPENSHIFT_ACTION_HOOKS_DIR/deploy
+$OPENSHIFT_ACTION_HOOKS_DIR/deploy << 'EOF'
+pushd ${OPENSHIFT_REPO_DIR} > /dev/null
+# Run db:migrate
+#bundle exec rake db:migrate RAILS_ENV="production"
+# Precompile the assets
+bundle exec rake assets:precompile RAILS_ENV="production"
+popd > /dev/null
+EOF
+
+### unicorn
+gem install unicorn
+# setup the start script
+echo > $OPENSHIFT_ACTION_HOOKS_DIR/start
+$OPENSHIFT_ACTION_HOOKS_DIR/start << 'EOF'
+#!/bin/bash
+export PATH=$OPENSHIFT_RUNTIME_DIR/bin:$PATH
+unicorn -l $OPENSHIFT_INTERNAL_IP:$OPENSHIFT_INTERNAL_PORT -E "production" -D
+EOF
+# setup the stop script
+echo > $OPENSHIFT_ACTION_HOOKS_DIR/stop
+$OPENSHIFT_ACTION_HOOKS_DIR/stop << 'EOF'
+#!/bin/bash
+export PATH=$OPENSHIFT_RUNTIME_DIR/bin:$PATH
+kill -s QUIT `ps -ef | grep "unicorn master" | grep -v grep | awk "{ print $2 }"`
+EOF
